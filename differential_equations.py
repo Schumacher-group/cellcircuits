998a218e53818c140ae6711a89eb8a6940730426
Georg Eichler <georg_eichler@web.de>
2024-07-31 16:52:01 +0100

    Complete outsorcing of all functions for calculating ODE's and signals into a differential file

---
+++ differential_equations.py
@@ -0,0 +1,105 @@
+import numpy as np
+from parameters import *
+from analysis import CSF_PDGF_steady
+
+#Outpust list of gradients, state encompasses concentrations for mF, M, CSF and PDGF as cells per ml
+def myofib_macro(state, t): # outputs list of gradients
+    
+    mF, M, CSF, PDGF = state
+
+    d_mF_dt = mF * (lambda1 * (PDGF / (k1 + PDGF)) * (1 - mF / K) - mu1)
+    d_M_dt = M * (lambda2 * (CSF / (k2+CSF)) - mu2)
+    d_CSF_dt = beta1 * mF - alpha1 * M * (CSF / (k2 + CSF)) - gamma * CSF
+    d_PDGF_dt = beta2 * M + beta3 * mF - alpha2 * mF * (PDGF / (k1 + PDGF))- gamma * PDGF
+
+    return [d_mF_dt, d_M_dt, d_CSF_dt, d_PDGF_dt]
+
+
+def mF_M_rates(state, t):
+    mF, M = state
+    
+    CSF, PDGF = CSF_PDGF_steady([mF, M])
+    d_mF_dt = mF * (lambda1 * (PDGF / (k1 + PDGF)) * (1 - mF / K) - mu1)
+    d_M_dt = M * (lambda2 * (CSF / (k2 + CSF)) - mu2)
+    return [d_mF_dt, d_M_dt]
+
+def rev_mf_M_rates(state, t):
+    derivatives = mF_M_rates(state, t)
+    return [-d for d in derivatives]
+
+#outputs reverse derivative, state encompasses mF, M concentrations 
+def myofib_macro_ODE_reverse(state, t):
+    derivatives = myofib_macro(state, t)
+    
+    #checking if concentrations are in a reasonable range
+    return [-d for d in derivatives] if all(0 <= x <= 10**7 for x in state) else [0, 0]
+
+
+#Step function
+def theta(t):
+    return 1 if t >= 0 else 0
+
+#Signal functions
+def transient(t):
+    return A_0*(theta(t) - theta(t - 2))
+
+def repetetive(t):
+    return transient(t) + transient(t - 4)
+
+def prolonged(t):
+    return A_0 * (theta(t) - theta(t - 4))
+
+def sharp_signal(t):
+    return (A_0 /0.2) * (theta(t) - theta(t - 0.2))
+
+def one_signal(t, delta_t):
+    return (A_0 * 4/ delta_t) * (theta(t) - theta(t - delta_t))
+
+#Block signals
+def blocks1(t):
+    return 0.25 * one_signal(t, 8) + 0.35 * one_signal(t - 8, 2) + 0.4 * one_signal(t - 10,2 )
+
+def blocks2(t):
+    return 0.6 * one_signal(t, 20) + 0.3 * one_signal(t - 20, 1) + 0.1 * one_signal(t - 21, 3)
+
+def blocks3(t):
+    return 0.5 * one_signal(t, 2) + 0.3 * one_signal(t - 2, 2) + 0.2 * one_signal(t - 4, 2)
+
+
+#Adjusted derivatives with signals
+#added signal increaces macrophage rate
+def transient_derivatives(state, t):
+    derivatives = mF_M_rates(state, t)
+    derivatives[1] += transient(t)
+    return derivatives
+
+def repetitive_derivatives(state, t):
+    derivatives = mF_M_rates(state, t)
+    derivatives[1] += repetetive(t)
+    return derivatives
+
+def prolonged_derivatives(state, t):
+    derivatives = mF_M_rates(state, t)
+    derivatives[1] += prolonged(t)
+    return derivatives
+
+def sharp_derivatives(state, t):
+    derivatives = mF_M_rates(state, t)
+    derivatives[1] += sharp_signal(t)
+    return derivatives
+
+#Block derivatives
+def blocks1_derivatives(state, t):
+    derivatives = mF_M_rates(state, t)
+    derivatives[1] += blocks1(t)
+    return derivatives
+
+def blocks2_derivatives(state, t):
+    derivatives = mF_M_rates(state, t)
+    derivatives[1] += blocks2(t)
+    return derivatives
+
+def blocks3_derivatives(state, t):
+    derivatives = mF_M_rates(state, t)
+    derivatives[1] += blocks3(t)
+    return derivatives
