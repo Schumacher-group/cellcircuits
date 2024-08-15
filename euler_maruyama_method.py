67f7eefd2c03e61fadcf6d8a75b5eb0e13bfc709
Georg Eichler <georg_eichler@web.de>
2024-08-15 16:07:09 +0100

    Define function for multiple simulations of euler_maruyama approximation

--- euler_maruyama_method.py
+++ euler_maruyama_method.py
@@ -5,20 +5,26 @@
 
 import numpy as np
 
-def euler_maruyama(t_trajectory, x0, deterministic_derivative, noise):
-    t0 = t_trajectory[0]
-    dt = (t_trajectory[-1] - t0)/(t_trajectory.size)
-    sqrt_dt = np.sqrt(dt)
+def euler_maruyama(deterministic_derivative, noise_function ,t_steps, x0, dt, sqrt_dt):
 
-    x = np.zeros((t_trajectory.size, 2))
+    x = np.zeros((t_steps.size, 2))
     x[0] = x0
 
-    t_steps = np.linspace(t0, t_trajectory[-1], t_trajectory.size)[1:]
+    noise_terms = np.array([noise_function(t) for t in t_steps])
 
-    noise_terms = np.array([noise(t) for t in t_steps])
-
-    for k in range(1, t_trajectory.size):
+    for k in range(1, t_steps.size):
         deterministic_term = np.array(deterministic_derivative(x[k-1], t_steps[k-1]))
         x[k] = x[k-1] + dt * deterministic_term + sqrt_dt * np.array([0, noise_terms[k-1]])
     
     return x
+
+def simulate_euler_maruyama(deterministic_derivative, noise_function, t_trajectory, x0, num_steps, axis):
+    t0 = t_trajectory[0]
+    dt = (t_trajectory[-1] - t0)/(t_trajectory.size)
+    sqrt_dt = np.sqrt(dt)
+
+    t_steps = np.linspace(t0, t_trajectory[-1], t_trajectory.size)
+
+    for _ in range(num_steps):
+        x = euler_maruyama(deterministic_derivative, noise_function, t_steps, x0, dt, sqrt_dt)
+        axis.plot(x[:,0], x[:,1])
