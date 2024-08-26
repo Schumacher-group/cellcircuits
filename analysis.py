bbbf61b3a3e8add07d13d7e42bd78fa6c8e2f8f8
Georg Eichler <georg_eichler@web.de>
2024-08-26 14:00:39 +0100

    Changed find_first_crossing_index to only take one interpolated function as input

--- analysis.py
+++ analysis.py
@@ -185,18 +185,18 @@ def check_hot_fibrosis(end_point, separatrix_left, separatrix_right):
 
 
 #Determine first crossing indices for integrated trajectory
-def find_first_crossing_index(x, left_interp, right_interp):
+def find_first_crossing_index(x, interpolation):
     
-    left_values = left_interp(x[:, 0])
-    right_values = right_interp(x[:, 0])
+    interpolation_values = interpolation(x[:, 0])
 
     #check if the trajectory crosses the separatrix
-    crosses = (x[:, 1] < left_values) | (x[:, 1] > right_values)
+    crosses = (x[:, 1] > interpolation_values)
 
     #find first index where crossing occours
     crossing_indices = np.where(crosses)[0]
 
     return crossing_indices[0] if crossing_indices.size > 0 else -1
+
 
 def time_taken(traj, t, hotfibrosis_mF_M, unstable_fixed_point_mF_M):
     end_point = [0, 0]
