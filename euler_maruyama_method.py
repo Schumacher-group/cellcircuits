9c784cee7875eb9a971058ef0184527e42d142dc
Georg Eichler <georg_eichler@web.de>
2024-09-09 22:53:21 +0100

    Added the small time step variable dt to the function inputs

--- euler_maruyama_method.py
+++ euler_maruyama_method.py
@@ -7,32 +7,32 @@ Our signal function is given as [dmF, dM + signal + noise], so we need to apply 
  
 import numpy as np
 
-def euler_maruyama(deterministic_derivative, noise_function ,t_steps, x0, dt, sqrt_dt):
+def euler_maruyama(deterministic_derivative, noise_function ,t_steps, x0, dt):
 
     x = np.zeros((t_steps.size, 2))
     x[0] = x0
 
-    noise_terms = noise_function(t_steps)
+    noise_terms = noise_function(t_steps, dt)
 
     for k in range(1, t_steps.size):
         deterministic_term = np.array(deterministic_derivative(x[k-1], t_steps[k-1]))
         #noise affects only the macrophages influx (second entry)
-        x[k] = x[k-1] + dt * deterministic_term + sqrt_dt * np.array([0, noise_terms[k-1]])
+        x[k] = x[k-1] + dt * deterministic_term + np.array([0, noise_terms[k-1]])
     
     return x[-1], x
+
 
 #only used for non-parallelized version
 def simulate_euler_maruyama(deterministic_derivative, noise_function, t_trajectory, x0, num_sim, axis):
     t0 = t_trajectory[0]
     dt = (t_trajectory[-1] - t0)/(t_trajectory.size)
-    sqrt_dt = np.sqrt(dt)
 
     t_steps = np.linspace(t0, t_trajectory[-1], t_trajectory.size)
 
     end_points = np.zeros((num_sim, 2))
 
     for num in range(num_sim):
-        end_point, x = euler_maruyama(deterministic_derivative, noise_function, t_steps, x0, dt, sqrt_dt)
+        end_point, x = euler_maruyama(deterministic_derivative, noise_function, t_steps, x0, dt)
 
         end_points[num] = end_point
         axis.plot(x[:,0], x[:,1])
@@ -43,10 +43,9 @@ def simulate_euler_maruyama(deterministic_derivative, noise_function, t_trajecto
 def single_euler_maruyama_simulation(deterministic_derivative, noise_function, t_trajectory, x0):
     t0 = t_trajectory[0]
     dt = (t_trajectory[-1] - t0)/(t_trajectory.size)
-    sqrt_dt = np.sqrt(dt)
 
     t_steps = np.linspace(t0, t_trajectory[-1], t_trajectory.size)
 
-    end_point, x = euler_maruyama(deterministic_derivative, noise_function, t_steps, x0, dt, sqrt_dt)
+    end_point, x = euler_maruyama(deterministic_derivative, noise_function, t_steps, x0, dt)
 
     return end_point, x
