75246e1e51522542278e2f6a505944a3e807b57f
Georg Eichler <georg_eichler@web.de>
2024-09-10 11:17:39 +0100

    Added gamma noise

--- Signal_functions.py
+++ Signal_functions.py
@@ -9,7 +9,7 @@ Amplitude: cells/day
 '''
 class Signal:
     def __init__(self, name = 'Input Signal', start_points = [0], durations = [1], amplitudes = [1],
-                normal_standard_deviations = [0], poisson_lams = [0]):
+                normal_standard_deviations = [0], poisson_lams = [0], gamma_alphas = [0], gamma_betas = [0]):
         # convert all arrays to numpy arrays
         self.name = name
         self.start_points = np.array(start_points)
@@ -26,6 +26,16 @@ class Signal:
             self.poisson_lams = np.zeros_like(amplitudes)
         else:
             self.poisson_lams = np.array(poisson_lams)
+
+        if isinstance(gamma_alphas, list) and gamma_alphas == [0]:
+            self.gamma_alphas = np.zeros_like(amplitudes)
+        else:
+            self.gamma_alphas = np.array(gamma_alphas)
+
+        if isinstance(gamma_betas, list) and gamma_betas == [0]:
+            self.gamma_betas = np.zeros_like(amplitudes)
+        else:
+            self.gamma_betas = np.array(gamma_betas)
 
     def __repr__(self):
         return f'{self.name}'
@@ -85,6 +95,7 @@ class Signal:
             total_noise += self.gaussian_signal(start, duration, std, t, dt)
         return total_noise
     
+    #Poisson noise
     def poisson_signal(self, start, duration, lam, t, dt):
         is_scalar = np.isscalar(t)
         t = np.array(t)
@@ -98,6 +109,23 @@ class Signal:
         total_noise = np.zeros_like(t)
         for start, duration, lam in zip(self.start_points, self.durations, self.poisson_lams):
             total_noise += self.poisson_signal(start, duration, lam, t, dt)
+        return total_noise
+    
+    #Gamma noise
+    def gamma_signal(self, start, duration, alpha, beta, t, dt):
+        is_scalar = np.isscalar(t)
+        t = np.array(t)
+        #beta is intended to be rate parameter but np implementation takes scale parameter theta = 1/beta
+        noise = np.random.gamma(dt * alpha, 1/beta, t.size) * (self.theta(t - start) - self.theta(t - (start + duration)))
+        
+        if is_scalar:
+            return noise.item()
+        return noise
+    
+    def gamma_noise_function(self, t, dt):
+        total_noise = np.zeros_like(t)
+        for start, duration, alpha, beta in zip(self.start_points, self.durations, self.gamma_alphas, self.gamma_betas):
+            total_noise += self.gamma_signal(start, duration, alpha, beta, t, dt)
         return total_noise
 
 
