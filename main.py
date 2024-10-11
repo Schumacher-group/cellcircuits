1dcc5058f048224335b31aacf5c231062ae3518b
Georg Eichler <georg_eichler@web.de>
2024-10-11 21:58:55 +0100

    New amplitudes values tested

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from parameters import *
 
 def main():
     #time vectors 
-    t_trajectory = np.linspace(0, 100, 4001)
+    t_trajectory = np.linspace(0, 100, 10001)
     t_separatrix = np.linspace(0, 800, 1000)
 
     #Initial mF and M concentration
@@ -27,11 +27,11 @@ def main():
     xsmooth = [10**5.7, 10**5.85]
     ysmooth = [nullcline_mF(pt)[1] for pt in xsmooth]
 
-    plotting.plot_nullclines_fixed_points_separatrix(mFM_space, mFnull1, mFnull2, mFnull3, xsmooth, ysmooth, t_separatrix = t_separatrix)
+    #plotting.plot_nullclines_fixed_points_separatrix(mFM_space, mFnull1, mFnull2, mFnull3, xsmooth, ysmooth, t_separatrix = t_separatrix)
 
-    plotting.plot_streamlines(mFM_space, t_separatrix)
+    #plotting.plot_streamlines(mFM_space, t_separatrix)
 
-    amplitudes = [i*A_0 for i in np.arange(0.01, 0.11, 0.01)]
+    amplitudes = [i*A_0 for i in np.arange(0.01, 10.01, 0.01)]
     plotting.amplitude_duration_dependence_for_hot_fibrosis(mFM_space, t_trajectory, t_separatrix, x_initial, amplitudes)
 
 
@@ -43,7 +43,7 @@ def main():
     gamma_std = 0.01*A_0
 
     #Gamma parameters in terms of mena and variance are (alpha, beta) = (mu**2/sigma**2, mu/sigma**2)
-    transient_signal = Signal(name = 'Transient signal', start_points = [0], durations = [2], amplitudes = [A_0])
+    transient_signal = Signal(name = 'Transient signal', start_points = [0], durations = [2], amplitudes = [10*A_0])
     prolonged_signal = Signal(name = 'Prolonged signal', start_points = [0], durations = [4], amplitudes = [A_0])
     repetitive_signal = Signal(name = 'Repetitive signal', start_points = [0,30], durations = [2,2], amplitudes = [A_0, 0.1*A_0])
     random_transient_signal1 = Signal(name = 'Random signal', start_points = [0], durations = [8], amplitudes = [0],
@@ -63,14 +63,14 @@ def main():
                                      normal_standard_deviations = [normal_std], poisson_lams = [poisson_lam],
                                      gamma_alphas = [gamma_mean**2/gamma_std**2], gamma_betas = [gamma_mean/gamma_std**2])
 
-    plotting.signals_and_trajectories(mFM_space, t_trajectory, t_separatrix, x_initial, signal = transient_signal)
+    #plotting.signals_and_trajectories(mFM_space, t_trajectory, t_separatrix, x_initial, signal = transient_signal)
     #plotting.signals_and_trajectories(mFM_space, t_trajectory, t_separatrix, x_initial, signal = prolonged_signal)
     #plotting.signals_and_trajectories(mFM_space, t_trajectory, t_separatrix, x_initial, signal = repetitive_signal)
 
     noise_type = 'gamma'
 
     num_sim = 10
-    plotting.plot_random_signal_trajectory_fibrosis_count(mFM_space, t_trajectory, t_separatrix, x_initial, signal = random_transient_signal1, num_sim = num_sim, noise_type=noise_type)
+    #plotting.plot_random_signal_trajectory_fibrosis_count(mFM_space, t_trajectory, t_separatrix, x_initial, signal = random_transient_signal1, num_sim = num_sim, noise_type=noise_type)
     #plotting.plot_random_signal_trajectory_fibrosis_count(mFM_space, t_trajectory, t_separatrix, x_initial, signal = random_transient_signal2, num_sim = num_sim, noise_type=noise_type)
     #plotting.plot_random_signal_trajectory_fibrosis_count(mFM_space, t_trajectory, t_separatrix, x_initial, signal = random_transient_signal3, num_sim = num_sim, noise_type=noise_type)
 
@@ -81,11 +81,11 @@ def main():
     gamma_means = [0.1*A_0]
     gamma_standard_deviations = [i*A_0 for i in np.arange(0.01, 0.1, 0.01)]
 
-    
+    '''
     plotting.plot_fibrosis_ratios(mFM_space, t_trajectory, t_separatrix, x_initial, start_point = 0, duration = 8, amplitude = 0, num_sim = num_sim, noise_type = noise_type,
                                 standard_deviations = gaussian_standard_deviations, poisson_lams = poisson_lams, gamma_means = gamma_means,
                                   gamma_standard_deviations = gamma_standard_deviations)
-    
+    '''
     #Depict the plots
     show()
 
